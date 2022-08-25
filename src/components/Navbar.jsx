e918eb6b768831c00c432fac86b500c743f57414
Mohamed Soliman <60075091+moeabraham@users.noreply.github.com>
2022-08-24 19:52:06 -0500

    adds link to resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -102,7 +102,7 @@ const Navbar = () => {
                 </li>
                 <li className="w-[160px] h-[60px] flex justify-between items center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
                     <a className='flex justify-between items-center w-full text-gray-300'
-                    href="https://drive.google.com/file/d/1oOlQPIEs0w-sZieeDkxF7eRhZwouwgYI/view?usp=sharing"
+                    href="https://drive.google.com/file/d/1N3FBGiX1_yFotJfKCJaslyFsTn_P_qB6/view?usp=sharing"
                     >
                         Resume <BsFillPersonLinesFill size={30} />
                         {/* resume <Resume /> */}
